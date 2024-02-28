d3f11c1421e909c08819b8e09a5d13c1fad90c49
skatox <migueluseche@skatox.com>
2024-02-28 11:56:59 -0500

    feat: show or hide post's date in the list

--- src/components/frontend/components/DisplayPost.js
+++ src/components/frontend/components/DisplayPost.js
@@ -1,7 +1,26 @@
-const DisplayPost = ( { post } ) => {
+/**
+ * WordPress dependencies
+ */
+import {useContext} from '@wordpress/element';
+import {dateI18n, getSettings} from '@wordpress/date';
+
+/**
+ * Internal dependencies
+ */
+import {ConfigContext} from '../context/ConfigContext';
+
+const DisplayPost = ({post}) => {
+	const {config} = useContext(ConfigContext);
+  const dateSettings = getSettings();
+
 	return (
-		<a href={ post.permalink } title={ post.post_title }>
-			{ post.post_title }
+		<a href={post.permalink} title={post.post_title}>
+			{post.post_title}
+			{config.show_post_date ? (
+				<span className="post-date">{dateI18n(dateSettings?.formats?.date, post.post_date)}</span>
+			) : (
+				''
+			)}
 		</a>
 	);
 };
--- src/tests/components/DisplayPost.test.js
+++ src/tests/components/DisplayPost.test.js
@@ -1,25 +1,69 @@
 // DisplayPost.test.js
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+
 import '@testing-library/jest-dom/extend-expect';
+import {ConfigContext, defaultConfig} from '../../components/frontend/context/ConfigContext';
 import DisplayPost from '../../components/frontend/components/DisplayPost';
 
-describe( 'Post', () => {
-	test( 'should display the post link', () => {
-		const post = {
-			ID: 1,
-			post_title: 'Test Title',
-			permalink: 'test-permalink',
-		};
+const animationFunction = jest.fn();
 
-		render( <DisplayPost post={ post } /> );
+jest.mock('@wordpress/date', () => ({
+	dateI18n: (format, date) => date,
+	getSettings: jest.fn().mockReturnValue({formats: {date: 'Y-m-d'}}),
+}));
 
-		expect( screen.getByText( post.post_title ) ).toHaveTextContent(
+const post = {
+	ID: 1,
+	post_title: 'Test Title',
+	post_date: '2021-01-01 00:00:00',
+	permalink: 'test-permalink',
+};
+
+describe('Post', () => {
+	beforeAll(() => {
+		jest.resetAllMocks();
+	});
+
+	test('should display the post link', () => {
+		const config = defaultConfig;
+
+		render(
+			<ConfigContext.Provider value={{config, animationFunction}}>
+				<DisplayPost post={post}/>
+			</ConfigContext.Provider>
+		);
+
+		expect(screen.getByText(post.post_title)).toHaveTextContent(
 			post.post_title
 		);
 
-		const postLinkElement = screen.getByRole( 'link', {
+		const postLinkElement = screen.getByRole('link', {
 			href: post.permalink,
-		} );
-		expect( postLinkElement ).toBeInTheDocument();
-	} );
-} );
+		});
+		expect(postLinkElement).toBeInTheDocument();
+	});
+
+	test('should display the post link with date', () => {
+		const config = {...defaultConfig, show_post_date: true};
+
+		const { getByText}=render(
+			<ConfigContext.Provider value={{config, animationFunction}}>
+				<DisplayPost post={post}/>
+			</ConfigContext.Provider>
+		);
+
+		expect(getByText(post.post_date)).toBeInTheDocument();
+	});
+
+	test('should not display the post link with date', () => {
+		const config = {...defaultConfig, show_post_date: false};
+
+		const { queryByText}=render(
+			<ConfigContext.Provider value={{config, animationFunction}}>
+				<DisplayPost post={post}/>
+			</ConfigContext.Provider>
+		);
+
+		expect(queryByText(post.post_date)).not.toBeInTheDocument();
+	});
+});
